95c9fa22a82aafe88f8b5279a4a0a44d6b392897
Alberto Bietti <alberto.bietti@gmail.com>
2011-05-03 00:36:11 +0200

    brain added to controverse (no visual effects yet)

--- controverse/main.js
+++ controverse/main.js
@@ -13,15 +13,36 @@ var ultimatum = function() {
 	
 };
 
+function apply(area, hoverimg, text) {
+	$('#area' + area).mouseover(function() {
+		$('#brainimg').attr('src', hoverimg);
+		$('#brainarea p').html(text);
+		$('#link' + area).addClass('empha');
+	}).mouseout(function() {
+		$('#brainimg').attr('src', "images/brain.png");
+		$('#brainarea p').html('<br />');
+		$('#link' + area).removeClass('empha');
+	}).click(function(e) {
+		e.preventDefault();
+		$.colorbox({width: "60%", height: "80%", close: "fermer", href: $('#link'+area).attr('href')})
+	});
+}
+
 var controverse = function() {
 	$('#main').hide();
 	$('#main').load('controverse.html', function() {
 		$('.boxed').colorbox({width: "60%", height: "80%", close: "fermer"});
+		apply(1, 'images/brain.png', 'Existence de la Neuroéconomie');
+		apply(2, 'images/brain.png', 'Apport de la Neuroéconomie');
+		apply(3, 'images/brain.png', 'Problèmes éthiques');
+		apply(4, 'images/brain.png', 'Neuromarketing');
+		apply(5, 'images/brain.png', 'Protocole expérimental');
 	});
 	$('#main').fadeIn('slow');
 
 };
 
+
 $(function() {
-	ultimatum();
+	controverse();
 });
