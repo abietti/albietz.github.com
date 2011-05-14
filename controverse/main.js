4f507a8037fe2eeef9a298a2f04d45792884f955
Alberto Bietti <alberto.bietti@gmail.com>
2011-05-14 14:03:36 +0200

    beginning of sections in articles

--- controverse/main.js
+++ controverse/main.js
@@ -2,11 +2,12 @@
 var ultimatum = function() {
 	$('#main').hide();
 	$('#main').load('ultimatum.html', function() {
-		$('#reponseOui,#reponseNon').children().hide();
+		$('#reponseOui,#reponseNon,#both').children().hide();
 		$('#ouinon a').click(function() {
 			//$('#reponseOui,#reponseNon').children().hide();
 			$('#ouinon').hide();
 			$('#reponse'+$(this).html()).children().slideDown();
+			$('#both').children().slideDown();
 		});
 	});
 	$('#main').fadeIn('slow');
@@ -25,16 +26,46 @@ function apply(area, hoverimg, text) {
 		$('#link' + area).removeClass('empha');
 	}).click(function(e) {
 		e.preventDefault();
-		$.colorbox({width: "80%", height: "80%", close: "fermer", href: $('#link'+area).attr('href')})
+		$.colorbox({onComplete: compl, width: "80%", height: "80%", close: "fermer", href: $('#link'+area).attr('href')})
 	});
+}
+
+function compl() {
+	var q = $('#cboxLoadedContent').children().filter(':gt(0)');
+	q.hide();
+	$('#cboxLoadedContent').append('<p><a id="prec" href="">précédent</a> <a id="suiv" href="">suivant</a></p>');
+	$('#prec').hide();
+	$('#cboxLoadedContent').children().first().nextUntil('h2').show();
+	var titles = $('#cboxLoadedContent').children().filter('h2');
+	var size = titles.length;
+	var section = -1;
+	
+	var suiv = function(e) {
+		e.preventDefault();
+		q.hide().last().show();
+		section++;
+		titles.eq(section).show().nextUntil('h2').show();
+		$('prec').show();
+		if (section == size-1) $('suiv').hide();
+	};
+	var prec = function(e) {
+		e.preventDefault();
+		q.hide().last().show();
+		section--;
+		titles.eq(section).show().nextUntil('h2').show();
+		$('suiv').show();
+		if (section == 0) $('prec').hide();
+	};
+	$('#suiv').click(suiv);
+	$('#prec').click(prec);
 }
 
 var controverse = function() {
 	$('#main').hide();
 	$('#main').load('controverse.html', function() {
-		$('.boxed').colorbox({width: "80%", height: "80%", close: "fermer"});
+		$('.boxed').colorbox({onComplete: compl, width: "80%", height: "90%", close: "fermer"});
 		apply(1, 'images/brain_1.jpg', 'La neuroéconomie : simple label ou véritable discipline ?');
-		apply(2, 'images/brain_2.jpg', 'Genèse');
+		apply(2, 'images/brain_2.jpg', 'Neuroéconomie : une nouvelle discipline en réponse à la crise financière ? Pas vraiment...');
 		apply(3, 'images/brain_3.jpg', 'Nouveaux domaines de recherche donc nouveaux protocoles, expériences controversées ?');
 		apply(4, 'images/brain_4.jpg', "La neuroéconomie fait-elle de l'homme une machine cérébrale ?");
 		apply(5, 'images/brain_5.jpg', 'Economie ou neurosciences : qui bénéficie vraiment de la neuroéconomie ?');
@@ -54,5 +85,5 @@ var controverse = function() {
 
 
 $(function() {
-	ultimatum();
+	controverse();
 });
